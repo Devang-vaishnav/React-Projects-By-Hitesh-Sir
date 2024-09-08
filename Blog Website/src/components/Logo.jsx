8b2aea47a7fc6eed181eb4d92bfaf032cb903f36
Devang-vaishnav <devangvaishnav732001@gmail.com>
2024-09-08 11:23:55 +0530

    Saving Changes

---
+++ Blog Website/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = ({width = '100px'}) => {
+    return (
+        <div>
+            <img src='../../asset/blogs-high-resolution-logo-white.png' width={width}/>
+        </div>
+    );
+}
+
+export default Logo;
---
+++ Blog Website/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Container from './container/Container'
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+import Select from './Select'
+import RTE from './RTE'
+import Signup from './Signup'
+import PostCard from './PostCard'
+import Login from './Login'
+import Input from './Input'
+import Button from './Button'
+import AuthLayout from './AuthLayout'
+import PostForm from './Post-form/PostForm'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Select,
+    RTE,
+    Signup,
+    PostCard,
+    Login,
+    Input,
+    Button,
+    AuthLayout,
+    PostForm
+}
